84cb59dcb446f999872c821b5eb442055de14d7e
anUnnecessary <raihanshamil33@gmail.com>
2023-05-22 21:06:21 +0600

    banner

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
+import NavBar from '../Pages/Shared/NavBar/NavBar';
+import Banner from '../Pages/Banner/Banner';
 
 const Main = () => {
     return (
         <div>
+            <NavBar></NavBar>
+            <Banner></Banner>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
