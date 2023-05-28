1857c1d6052decdae62f0111689640a6b25c4cef
anUnnecessary <raihanshamil33@gmail.com>
2023-05-28 23:33:46 +0600

    social added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,6 +4,7 @@ import { AuthContext } from '../Provider/AuthProvider';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import Swal from 'sweetalert2';
+import SocialLogin from '../Shared/SocialLogin/SocialLogin';
 
 
 const Login = () => {
@@ -92,6 +93,7 @@ const Login = () => {
                             </div>
                         </form>
                         <p><small>New Here? <Link to="/signup">Create an account</Link> </small></p>
+                        <SocialLogin></SocialLogin>
                     </div>
                 </div>
             </div>
