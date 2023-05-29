ba52040821483e85bf92cfe5647e9215a23a550a
anUnnecessary <raihanshamil33@gmail.com>
2023-05-29 21:20:15 +0600

    admin authorized

--- src/Pages/Hooks/useCart.jsx
+++ src/Pages/Hooks/useCart.jsx
@@ -1,9 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
-import { useContext } from 'react';
-import { AuthContext } from '../Provider/AuthProvider';
+import useAuth from './useAuth'
+
 
 const useCart = () => {
-    const { user } = useContext(AuthContext);
+    const { user } = useAuth();
     const token = localStorage.getItem('access-token')
 
     const { refetch, data: cart = [] } = useQuery({
