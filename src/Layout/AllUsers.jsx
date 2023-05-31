4156272455658831d43434dc88eae88c3e939e9a
anUnnecessary <raihanshamil33@gmail.com>
2023-05-31 21:09:59 +0600

    added

--- src/Layout/AllUsers.jsx
+++ src/Layout/AllUsers.jsx
@@ -1,49 +1,48 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import { Helmet } from 'react-helmet-async'; 
-import { FaTrashAlt, FaUserShield } from 'react-icons/fa';
-import Swal from 'sweetalert2';
+import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../Pages/Hooks/useAxiosSecure";
+
+
 
 const AllUsers = () => {
+    const [axiosSecure] = useAxiosSecure();
     const { data: users = [], refetch } = useQuery(['users'], async () => {
-        const res = await fetch('http://localhost:5000/users')
-        return res.json();
+        const res = await axiosSecure.get('/users')
+        return res.data;
     })
 
-    const handleMakeAdmin = user => {
+    const handleMakeAdmin = user =>{
         fetch(`http://localhost:5000/users/admin/${user._id}`, {
             method: 'PATCH'
         })
-        .then(res=> res.json())
-        .then(data=> {
+        .then(res => res.json())
+        .then(data => {
             console.log(data)
             if(data.modifiedCount){
                 refetch();
                 Swal.fire({
                     position: 'top-end',
                     icon: 'success',
-                    title: `${user.name} is an Admin Now`,
+                    title: `${user.name} is an Admin Now!`,
                     showConfirmButton: false,
                     timer: 1500
                   })
             }
         })
-           
-        
-       
     }
-
 
     const handleDelete = user => {
 
     }
 
     return (
-        <div className='w-full'>
+        <div className="w-full">
             <Helmet>
-                <title>Bistro Boss | All Users</title>
+                <title>Bistro Boss | All users</title>
             </Helmet>
-            <h3 className="text-3xl font-semibold my-4">Total Users : {users.length}</h3>
+            <h3 className="text-3xl font-semibold my-4">Total Users: {users.length}</h3>
             <div className="overflow-x-auto">
                 <table className="table table-zebra w-full">
                     {/* head */}
@@ -52,7 +51,7 @@ const AllUsers = () => {
                             <th>#</th>
                             <th>Name</th>
                             <th>Email</th>
-                            <th>{users.role === 'admin' ? 'admin' : <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost bg-orange-600  text-white"><FaUserShield></FaUserShield></button>}</th>
+                            <th>Role</th>
                             <th>Action</th>
                         </tr>
                     </thead>
@@ -62,10 +61,14 @@ const AllUsers = () => {
                                 <th>{index + 1}</th>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
-                                <td>Blue</td>
+                                <td>{ user.role === 'admin' ? 'admin' :
+                                    <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost bg-orange-600  text-white"><FaUserShield></FaUserShield></button> 
+                                    }</td>
                                 <td><button onClick={() => handleDelete(user)} className="btn btn-ghost bg-red-600  text-white"><FaTrashAlt></FaTrashAlt></button></td>
                             </tr>)
                         }
+                        
+                        
                     </tbody>
                 </table>
             </div>
