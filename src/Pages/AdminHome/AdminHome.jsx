61d7ee5a3f64adab3130881d089175be0f165b2c
anUnnecessary <raihanshamil33@gmail.com>
2023-06-02 21:44:16 +0600

    axios updated

--- src/Pages/AdminHome/AdminHome.jsx
+++ src/Pages/AdminHome/AdminHome.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import useAuth from '../Hooks/useAuth';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosSecure from '../Hooks/useAxiosSecure';
 
 const AdminHome = () => {
     const {user} = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+
+    const {data: stats = {}} = useQuery({
+        queryKey: ['admin-stats'],
+        queryFn: async () => {
+            const res = await axiosSecure('/admin-stats');
+            return res.data;
+        }
+    })
+
     return (
         <div className='w-full m-4'>
-            <h2>Welcome back, {user.displayName}</h2>
+            <h2>Hi, Welcome back, {user.displayName}</h2>
         </div>
     );
 };
