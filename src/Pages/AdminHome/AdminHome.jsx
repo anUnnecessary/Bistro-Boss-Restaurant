adaafc79c53d86579b4b12a974d7395f6c4a04d1
anUnnecessary <raihanshamil33@gmail.com>
2023-06-02 22:50:34 +0600

    chart added here

--- src/Pages/AdminHome/AdminHome.jsx
+++ src/Pages/AdminHome/AdminHome.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import useAuth from '../Hooks/useAuth';
 import { useQuery } from '@tanstack/react-query';
 import useAxiosSecure from '../Hooks/useAxiosSecure';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
 
 const AdminHome = () => {
-    const {user} = useAuth();
+    const { user } = useAuth();
     const [axiosSecure] = useAxiosSecure();
 
-    const {data: stats = {}} = useQuery({
+    const { data: stats = {} } = useQuery({
         queryKey: ['admin-stats'],
         queryFn: async () => {
             const res = await axiosSecure('/admin-stats');
@@ -15,9 +16,103 @@ const AdminHome = () => {
         }
     })
 
+    const {data: chartData = [] } = useQuery({
+        queryKey: ['chart-data'],
+        queryFn: async() => {
+            const res = await axiosSecure('/order-stats')
+        }
+    })
+
+
+    const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+        ${x + width / 2}, ${y}
+        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+        Z`;
+      };
+      
+      const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+      
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+      };
+      
+
+
     return (
         <div className='w-full m-4'>
             <h2>Hi, Welcome back, {user.displayName}</h2>
+            <div className="stats shadow">
+
+                <div className="stat">
+                    <div className="stat-figure text-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                    </div>
+                    <div className="stat-title">Revenue</div>
+                    <div className="stat-value text-primary">${stats.revenue}</div>
+                    <div className="stat-desc">21% more than last month</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+                    </div>
+                    <div className="stat-title">New Users</div>
+                    <div className="stat-value text-secondary">{stats.users}</div>
+                    <div className="stat-desc">21% more than last month</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <div className="avatar online">
+                            <div className="w-16 rounded-full">
+                                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="stat-value">Menu Items</div>
+                    <div className="stat-title">{stats.products}</div>
+                    <div className="stat-desc text-secondary">31 tasks remaining</div>
+                </div>
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <div className="avatar online">
+                            <div className="w-16 rounded-full">
+                                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="stat-value">Orders</div>
+                    <div className="stat-title">{stats.orders}</div>
+                    <div className="stat-desc text-secondary">31 tasks remaining</div>
+                </div>
+            </div>
+            <div className='w-full'>
+                <div className='w-1/2'>
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Bar dataKey="uv" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                            {data.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                            ))}
+                        </Bar>
+                    </BarChart>
+                </div>
+            </div>
         </div>
     );
 };
